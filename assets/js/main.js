c2702e5d9678e28b2d485d7e45d94d3d3df23716
seahyunjin <seaheun08@naver.com>
2020-12-10 22:27:50 +0900

    init commit

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+/*
+	Retrospect by TEMPLATED
+Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+$(function () {
+    $(window).scroll(function () { //브라우저에 스크롤이 발생하는 순간부터
+        var curpos = $(window).scrollTop()+20; //스크롤바의 상단 위치값+20 보관
+        $(".sky").stop().animate({"top":curpos}); //스카이메뉴의 상단위치값 애니
+    });
+});
+(function($) {
+
+	skel
+		.breakpoints({
+			xlarge: '(max-width: 1680px)',
+			large: '(max-width: 1280px)',
+			medium: '(max-width: 980px)',
+			small: '(max-width: 736px)',
+			xsmall: '(max-width: 480px)'
+		});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Nav.
+			$('#nav')
+				.append('<a href="#nav" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right'
+				});
+
+	});
+
+})(jQuery);
